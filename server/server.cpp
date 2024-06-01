83261664abb0370e5c253b12123c438c375139d5
Josiah Moses <32985363+JosiahMoses@users.noreply.github.com>
2024-06-01 14:22:53 -0500

    Added initial server code commit

---
+++ server/server.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+
+using namespace std;
+
+int main()
+{
+    // Server Code will go here.. :)
+}
